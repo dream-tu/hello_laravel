ce46133b5f43376c5d64b654efe0f727176a4226
tu <php1007@163.com>
2020-12-14 22:37:06 +0800

    中文的错误提示信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,8 +20,8 @@ class UsersController extends Controller
     public function store(Request $request)
     {
         $this->validate($request,[
-            'name' => 'require|unique:users|max:50',
-            'email' => 'require|email|unique:users|max:255',
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
         return;
